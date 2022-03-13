96bf77431d3669ee6650c8580083cd83ab1a3669
lcenriquez <luiscarlos_enriquez@me.com>
2022-03-13 14:36:12 -0600

    style: set initialColorMode to system

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -1,8 +1,7 @@
 import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
-  initialColorMode: "light",
-  useSystemColorMode: true
+  initialColorMode: 'system'
 };
 
 const breakpoints = {
