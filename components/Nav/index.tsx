85d062db0fe8c981f73aac5265ff1f7ebbedee0c
lcenriquez <luiscarlos_enriquez@me.com>
2022-03-15 06:54:14 -0600

    feat: add link to home in Nav

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -32,13 +32,13 @@ export default function Nav() {
       <Flex
         bg={useColorModeValue('white', 'gray.800')}
         color={useColorModeValue('gray.600', 'white')}
-        minH="60px"
+        minH='60px'
         py={{ base: 2 }}
         px={{ base: 4 }}
         borderBottom={1}
-        borderStyle="solid"
+        borderStyle='solid'
         borderColor={useColorModeValue('gray.200', 'gray.900')}
-        align="center">
+        align='center'>
         <Flex
           flex={{ base: 1, md: 'auto' }}
           ml={{ base: -2 }}
@@ -48,18 +48,20 @@ export default function Nav() {
             icon={
               isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
             }
-            variant="ghost"
-            aria-label="Toggle Navigation"
+            variant='ghost'
+            aria-label='Toggle Navigation'
           />
         </Flex>
         <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-          <Text
-            textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
-            fontFamily="heading"
-            color={useColorModeValue('gray.800', 'white')}>
-            Logo
-          </Text>
-
+          <Link href='/'>
+            <Text
+              textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+              fontFamily='heading'
+              fontWeight='bold'
+              color={useColorModeValue('gray.800', 'white')}>
+              SAF Hub
+            </Text>
+          </Link>
           <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
             <DesktopNav />
           </Flex>
@@ -67,27 +69,27 @@ export default function Nav() {
 
         <Stack
           flex={{ base: 1, md: 0 }}
-          justify="flex-end"
-          alignItems="center"
-          direction="row"
+          justify='flex-end'
+          alignItems='center'
+          direction='row'
           spacing={6}>
           <ThemeSwitch />
           <Button
-            as="a"
-            fontSize="sm"
+            as='a'
+            fontSize='sm'
             fontWeight={400}
-            variant="link"
-            href="#">
+            variant='link'
+            href='#'>
             Sign In
           </Button>
           <Button
-            as="a"
+            as='a'
             display={{ base: 'none', md: 'inline-flex' }}
-            fontSize="sm"
+            fontSize='sm'
             fontWeight={600}
-            color="white"
-            bg="pink.400"
-            href="#"
+            color='white'
+            bg='pink.400'
+            href='#'
             _hover={{
               bg: 'pink.300',
             }}>
@@ -109,15 +111,15 @@ const DesktopNav = () => {
   const popoverContentBgColor = useColorModeValue('white', 'gray.800');
 
   return (
-    <Stack direction="row" spacing={4}>
+    <Stack direction='row' spacing={4}>
       {NAV_ITEMS.map((navItem) => (
         <Box key={navItem.label}>
-          <Popover trigger="hover" placement="bottom-start">
+          <Popover trigger='hover' placement='bottom-start'>
             <PopoverTrigger>
               <Link
                 p={2}
                 href={navItem.href ?? '#'}
-                fontSize="sm"
+                fontSize='sm'
                 fontWeight={500}
                 color={linkColor}
                 _hover={{
@@ -131,11 +133,11 @@ const DesktopNav = () => {
             {navItem.children && (
               <PopoverContent
                 border={0}
-                boxShadow="xl"
+                boxShadow='xl'
                 bg={popoverContentBgColor}
                 p={4}
-                rounded="xl"
-                minW="sm">
+                rounded='xl'
+                minW='sm'>
                 <Stack>
                   {navItem.children.map((child) => (
                     <DesktopSubNav key={child.label} {...child} />
@@ -154,30 +156,30 @@ const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
   return (
     <Link
       href={href}
-      role="group"
-      display="block"
+      role='group'
+      display='block'
       p={2}
-      rounded="md"
+      rounded='md'
       _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}>
-      <Stack direction="row" align="center">
+      <Stack direction='row' align='center'>
         <Box>
           <Text
-            transition="all .3s ease"
+            transition='all .3s ease'
             _groupHover={{ color: 'pink.400' }}
             fontWeight={500}>
             {label}
           </Text>
-          <Text fontSize="sm">{subLabel}</Text>
+          <Text fontSize='sm'>{subLabel}</Text>
         </Box>
         <Flex
-          transition="all .3s ease"
-          transform="translateX(-10px)"
+          transition='all .3s ease'
+          transform='translateX(-10px)'
           opacity={0}
           _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
-          justify="flex-end"
-          align="center"
+          justify='flex-end'
+          align='center'
           flex={1}>
-          <Icon color="pink.400" w={5} h={5} as={ChevronRightIcon} />
+          <Icon color='pink.400' w={5} h={5} as={ChevronRightIcon} />
         </Flex>
       </Stack>
     </Link>
@@ -206,8 +208,8 @@ const MobileNavItem = ({ label, children, href }: NavItem) => {
         py={2}
         as={Link}
         href={href ?? '#'}
-        justify="space-between"
-        align="center"
+        justify='space-between'
+        align='center'
         _hover={{
           textDecoration: 'none',
         }}>
@@ -219,7 +221,7 @@ const MobileNavItem = ({ label, children, href }: NavItem) => {
         {children && (
           <Icon
             as={ChevronDownIcon}
-            transition="all .25s ease-in-out"
+            transition='all .25s ease-in-out'
             transform={isOpen ? 'rotate(180deg)' : ''}
             w={6}
             h={6}
@@ -232,9 +234,9 @@ const MobileNavItem = ({ label, children, href }: NavItem) => {
           mt={2}
           pl={4}
           borderLeft={1}
-          borderStyle="solid"
+          borderStyle='solid'
           borderColor={useColorModeValue('gray.200', 'gray.700')}
-          align="start">
+          align='start'>
           {children &&
             children.map((child) => (
               <Link key={child.label} py={2} href={child.href}>
