44556f95f7e87e67459412fe6fd606b96f229795
lcenriquez <luiscarlos_enriquez@me.com>
2022-03-18 20:35:00 -0600

    feat: open SocialButton links in a new tab

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -48,6 +48,7 @@ const SocialButton = ({
       cursor="pointer"
       as="a"
       href={href}
+      target='_blank'
       display="inline-flex"
       alignItems="center"
       justifyContent="center"
