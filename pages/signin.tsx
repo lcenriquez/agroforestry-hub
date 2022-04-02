13e7bf0f98436b891e50473e5757957c7aea4a9a
lcenriquez <luiscarlos_enriquez@me.com>
2022-04-02 16:43:31 -0600

    feat: redirect user if auth is successful

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -12,14 +12,37 @@ import {
   Text,
   useColorModeValue,
 } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useAuth } from '../contexts/AuthContext';
 
-export default function SimpleCard() {
+export default function SignIn() {
+  const router = useRouter();
+  const [ input, setInput ] = useState({ email: '', password: '' });
+  const { authUser, loading, signIn } = useAuth();
+
+  useEffect(() => {
+    if (!loading && authUser)
+      router.push('/')
+  }, [authUser, loading, router])
+
+  function handleSumbit(event: any) {
+    event.preventDefault();
+    signIn(input.email, input.password);
+  }
+
+  function handleChange(event: any) {
+    event.preventDefault();
+    setInput({...input, [event.target.name]: event.target.value})
+  }
+
   return (
     <Flex
       minH={'100vh'}
       align={'center'}
       justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
+      bg={useColorModeValue('gray.50', 'gray.800')}
+    >
       <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
         <Stack align={'center'}>
           <Heading fontSize={'4xl'}>Sign in to your account</Heading>
@@ -31,34 +54,41 @@ export default function SimpleCard() {
           rounded={'lg'}
           bg={useColorModeValue('white', 'gray.700')}
           boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}>
-                <Checkbox>Remember me</Checkbox>
-                <Link color={'blue.400'}>Forgot password?</Link>
+          p={8}
+        >
+          <form onSubmit={(e) => handleSumbit(e)}>
+            <Stack spacing={4}>
+              <FormControl id='email'>
+                <FormLabel>Email address</FormLabel>
+                <Input type='email' name='email' onChange={(e) => handleChange(e)} />
+              </FormControl>
+              <FormControl id='password'>
+                <FormLabel>Password</FormLabel>
+                <Input type='password' name='password' onChange={(e) => handleChange(e)} />
+              </FormControl>
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: 'column', sm: 'row' }}
+                  align={'start'}
+                  justify={'space-between'}
+                >
+                  <Checkbox>Remember me</Checkbox>
+                  <Link color={'blue.400'}>Forgot password?</Link>
+                </Stack>
+                <Button
+                  type='submit'
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}
+                  isLoading={loading}
+                >
+                  Sign in
+                </Button>
               </Stack>
-              <Button
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}>
-                Sign in
-              </Button>
             </Stack>
-          </Stack>
+          </form>
         </Box>
       </Stack>
     </Flex>
