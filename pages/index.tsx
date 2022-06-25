b2b733406967392323fe413f4021c556f635ce7f
lcenriquez <luiscarlos_enriquez@me.com>
2022-06-25 16:38:58 -0500

    feat: use withPublicLayout in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,29 @@
 import { Container, Heading } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
-import { getSpecies } from '../adapters/firestore'
+import { getSpecies } from '../adapters/firestore';
 import SpeciesTable from '../components/Tables/SpeciesTable';
-import withLayout from '../hocs/withLayout';
-import type { NextPage } from 'next'
-
+import { withPublicLayout } from '../hocs/withLayout';
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  const [ species, setSpecies ] = useState<{}[]>([]);
+  const [species, setSpecies] = useState<{}[]>([]);
 
   useEffect(() => {
     const firstLoad = async () => {
       if (species.length === 0) setSpecies(await getSpecies());
-    }
+    };
     firstLoad();
-  },[species]);
+  }, [species]);
 
   return (
     <Container maxW='container.xl'>
       <Heading>Sistemas agroforestales</Heading>
       {species.length > 0 ? <SpeciesTable species={species} /> : null}
     </Container>
-  )
-}
+  );
+};
 
-// export async function getServerSideProps() {
-//   return {
-//     props: {}, // will be passed to the page component as props
-//   }
-// }
-
-export default withLayout(Home, 'Toda la información que necesitas sobre SAFs');
+export default withPublicLayout(
+  Home,
+  'Toda la información que necesitas sobre SAFs'
+);
