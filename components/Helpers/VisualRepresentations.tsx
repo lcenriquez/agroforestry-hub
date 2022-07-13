ca138869e7e13d7d06c6d4c0622237d29d393868
lcenriquez <luiscarlos_enriquez@me.com>
2022-07-12 21:44:34 -0500

    feat: add details column

--- components/Helpers/VisualRepresentations.tsx
+++ components/Helpers/VisualRepresentations.tsx
@@ -1,67 +1,134 @@
 import { Box, Tooltip } from '@chakra-ui/react';
-import { Atom, Butterfly, ForkKnife, Graph, Heartbeat, Horse, Leaf, Mountains, Question, Tree } from 'phosphor-react';
+import {
+  Atom,
+  Butterfly,
+  DropHalfBottom,
+  ForkKnife,
+  Graph,
+  Heartbeat,
+  Horse,
+  Leaf,
+  Mountains,
+  Question,
+  Snowflake,
+  Sun,
+  Tree,
+} from 'phosphor-react';
 
-export function EcologicalFunctionIconRepresentation({value}: any) {
+export function EcologicalFunctionIconRepresentation({ value }: any) {
   let icon;
-  switch(+value._id) {
+  switch (+value._id) {
     case 0:
-      icon = <Leaf /> // Biomass
+      icon = <Leaf />; // Biomass
       break;
     case 1:
-      icon = <Graph /> // Nitrogen fixation
+      icon = <Graph />; // Nitrogen fixation
       break;
     case 2:
-      icon = <Atom /> // Nutrient accumulation
+      icon = <Atom />; // Nutrient accumulation
       break;
     case 3:
-      icon = <Mountains /> // Erotion control
+      icon = <Mountains />; // Erotion control
       break;
     case 4:
-      icon = <Butterfly /> // Pollinators
+      icon = <Butterfly />; // Pollinators
       break;
     default:
-      icon = <Question />
+      icon = <Question />;
       break;
   }
 
   return (
     <Tooltip label={value.name.es_mx}>
-      <Box cursor='default' display='inline-flex' mx='1px'>{icon}</Box>
+      <Box cursor='default' display='inline-flex' mx='1px'>
+        {icon}
+      </Box>
     </Tooltip>
   );
 }
 
-export function AdditionalFunctionIconRepresentation({value}: any) {
+export function AdditionalFunctionIconRepresentation({ value }: any) {
   let icon;
-  switch(+value._id) {
+  switch (+value._id) {
     case 0:
-      icon = <ForkKnife /> // Food
+      icon = <ForkKnife />; // Food
       break;
     case 1:
-      icon = <Horse /> // Forage
+      icon = <Horse />; // Forage
       break;
     case 2:
-      icon = <Tree /> // Wood
+      icon = <Tree />; // Wood
       break;
     case 3:
-      icon = <Heartbeat /> // Medicinal
+      icon = <Heartbeat />; // Medicinal
       break;
     default:
-      icon = <Question />
+      icon = <Question />;
       break;
   }
 
   return (
     <Tooltip label={value.name.es_mx}>
-      <Box cursor='default' display='inline-flex' mx='1px'>{icon}</Box>
+      <Box cursor='default' display='inline-flex' mx='1px'>
+        {icon}
+      </Box>
     </Tooltip>
   );
 }
 
-export function SingleCharRepresentation({value, name}: any) {
+export function SingleCharRepresentation({ value, name }: any) {
   return (
     <Tooltip label={value.name.es_mx}>
-      <Box cursor='default' display='inline-flex' mx='1px'>{name || value._id}</Box>
+      <Box cursor='default' display='inline-flex' mx='1px'>
+        {name || value._id}
+      </Box>
+    </Tooltip>
+  );
+}
+
+export function DetailIconRepresentation({ idKey, value }: any) {
+  let icon;
+  let caption: string;
+  switch (idKey) {
+    case 'isFrostResistant':
+      icon = <Snowflake />;
+      caption = 'Resiste heladas';
+      break;
+    case 'lightPreference':
+      icon = <Sun />;
+      value === 'H'
+        ? (caption = 'Prefiere sol')
+        : value === 'M'
+        ? (caption = 'Prefiere media sombra')
+        : (caption = 'Prefiere sombra');
+      break;
+    case 'nutrientExtraction':
+      icon = <Atom />;
+      value === 'H'
+        ? (caption = 'Alta extracción de nutrientes')
+        : value === 'M'
+        ? (caption = 'Extracción media de nutrientes')
+        : (caption = 'Baja extracción de nutrientes');
+      break;
+    case 'humidityPreference':
+      icon = <DropHalfBottom />;
+      value === 'H'
+        ? (caption = 'Prefiere humedad alta')
+        : value === 'M'
+        ? (caption = 'Prefiere humedad media')
+        : (caption = 'Prefiere clima seco');
+      break;
+    default:
+      icon = <Question />;
+      caption = '';
+      break;
+  }
+
+  return (
+    <Tooltip label={caption}>
+      <Box cursor='default' display='inline-flex' mx='1px'>
+        {icon}
+      </Box>
     </Tooltip>
   );
 }
--- components/Tables/SpeciesTable.tsx
+++ components/Tables/SpeciesTable.tsx
@@ -1,12 +1,29 @@
-import { Table, TableCaption, Text, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react'
-import { AdditionalFunctionIconRepresentation, EcologicalFunctionIconRepresentation, SingleCharRepresentation } from '../Helpers/VisualRepresentations';
+import {
+  Table,
+  TableCaption,
+  Text,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from '@chakra-ui/react';
+import {
+  AdditionalFunctionIconRepresentation,
+  DetailIconRepresentation,
+  EcologicalFunctionIconRepresentation,
+  SingleCharRepresentation,
+} from '../Helpers/VisualRepresentations';
 
 export default function SpeciesTable({ species }: any) {
   console.log(species);
-  
+
   return (
     <Table variant='striped' size='sm'>
-      <TableCaption>Especies mostradas según región: mx. Todas las medidas están dadas en metros.</TableCaption>
+      <TableCaption>
+        Especies mostradas según región: mx. Todas las medidas están dadas en
+        metros.
+      </TableCaption>
       <Thead>
         <Tr>
           <Th>Nombre (científico y común)</Th>
@@ -16,6 +33,7 @@ export default function SpeciesTable({ species }: any) {
           <Th>Ancho de copa</Th>
           <Th>Funciones ecológicas</Th>
           <Th>Otras funciones</Th>
+          <Th>Detalles</Th>
         </Tr>
       </Thead>
       <Tbody>
@@ -24,15 +42,44 @@ export default function SpeciesTable({ species }: any) {
             <Tr key={sp._id}>
               <Td>
                 <Text>{`${sp.taxonomy.genus} ${sp.taxonomy.species}`}</Text>
-                <Text fontSize='0.7rem' fontStyle='italic'>{sp.commonNames.es_mx?.join(', ')}</Text>
+                <Text fontSize='0.7rem' fontStyle='italic'>
+                  {sp.commonNames.es_mx?.join(', ')}
+                </Text>
               </Td>
-              <Td>{sp.ecologicalZones.mx.map((z: any) => <SingleCharRepresentation key={z._id} value={z} />)}</Td>
-              <Td>{sp.stratums.map((s: any) => <SingleCharRepresentation key={s._id} value={s} />)}</Td>
+              <Td>
+                {sp.ecologicalZones.mx.map((z: any) => (
+                  <SingleCharRepresentation key={z._id} value={z} />
+                ))}
+              </Td>
+              <Td>
+                {sp.stratums.map((s: any) => (
+                  <SingleCharRepresentation key={s._id} value={s} />
+                ))}
+              </Td>
               <Td>{`${sp.height.min}-${sp.height.max}`}</Td>
               <Td>{`${sp.crownWidth.min}-${sp.crownWidth.max}`}</Td>
-              <Td>{sp.ecologicalFunctions.map((f: any) => <EcologicalFunctionIconRepresentation key={f._id} value={f} />)}</Td>
-              <Td>{sp.additionalFunctions.map((f: any) => <AdditionalFunctionIconRepresentation key={f._id} value={f} />)}</Td>
-            </Tr>)
+              <Td>
+                {sp.ecologicalFunctions.map((f: any) => (
+                  <EcologicalFunctionIconRepresentation key={f._id} value={f} />
+                ))}
+              </Td>
+              <Td>
+                {sp.additionalFunctions.map((f: any) => (
+                  <AdditionalFunctionIconRepresentation key={f._id} value={f} />
+                ))}
+              </Td>
+              <Td>
+                {sp.details &&
+                  Object.keys(sp.details).map((key: string) => (
+                    <DetailIconRepresentation
+                      key={key}
+                      idKey={key}
+                      value={sp.details[key]}
+                    />
+                  ))}
+              </Td>
+            </Tr>
+          );
         })}
       </Tbody>
     </Table>
